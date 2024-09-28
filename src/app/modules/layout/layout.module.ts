058c990b0904c89f1176b89cd27198b8f96263bd
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2024-09-27 19:48:47 -0500

    feat: :boom: Shared module is created and imported into the layout module

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -1,11 +1,9 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FooterComponent } from '@module/layout/footer/footer.component';
 import { LayoutRoutingModule } from '@module/layout/layout-routing.module';
 import { LayoutComponent } from '@module/layout/layout.component';
 import { NavbarComponent } from '@module/layout/navbar/navbar.component';
-import { IconsModule } from '@shared/icons/icons.module';
-import { OverlayscrollbarsModule } from 'overlayscrollbars-ngx';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -14,10 +12,8 @@ import { OverlayscrollbarsModule } from 'overlayscrollbars-ngx';
     FooterComponent
   ],
   imports: [
-    CommonModule,
     LayoutRoutingModule,
-    OverlayscrollbarsModule,
-    IconsModule
+    SharedModule
   ]
 })
 export class LayoutModule { }
