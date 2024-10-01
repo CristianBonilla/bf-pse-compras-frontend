60781a312873516d104a8f2d5a8b5db8b93cbf77
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2024-09-30 23:31:25 -0500

    :recycle: Svg icon module is added and shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -5,6 +6,7 @@ import { CoreModule } from '@core/core.module';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { AppComponent } from 'src/app/app.component';
 
@@ -19,10 +21,12 @@ import { AppComponent } from 'src/app/app.component';
     StoreModule.forRoot({}, {}),
     EffectsModule.forRoot([]),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
-    CoreModule
+    CoreModule,
+    AngularSvgIconModule.forRoot()
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideHttpClient(withFetch())
   ],
   bootstrap: [AppComponent]
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,23 +1,26 @@
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { TOASTR_GLOBAL_CONFIG } from '@shared/constants/toastr-config';
 import { IconsModule } from '@shared/icons/icons.module';
+import { SafeHtmlPipe } from '@shared/pipes/safe-html/safe-html.pipe';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 import { ToastrModule, ToastrService } from 'ngx-toastr';
 import { NgxTrimDirectiveModule } from 'ngx-trim-directive';
 import { OverlayscrollbarsModule } from 'overlayscrollbars-ngx';
-import { SafeHtmlPipe } from '@shared/pipes/safe-html/safe-html.pipe';
 
 @NgModule({
   declarations: [
     SafeHtmlPipe
   ],
   imports: [
-    ToastrModule.forRoot(TOASTR_GLOBAL_CONFIG),
+    ToastrModule.forRoot(TOASTR_GLOBAL_CONFIG)
   ],
   exports: [
     OverlayscrollbarsModule,
     IconsModule,
     NgxTrimDirectiveModule,
-    SafeHtmlPipe
+    SafeHtmlPipe,
+    AngularSvgIconModule
   ],
   providers: [ToastrService]
 })
