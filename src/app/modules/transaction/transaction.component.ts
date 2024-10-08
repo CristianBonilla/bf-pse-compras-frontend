34f54282644a649fd3315983da9892d87dbd2d16
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2024-10-08 11:18:26 -0500

    feat: :construction: Aggregate Transaction Module and Component

---
+++ src/app/modules/transaction/transaction.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bf-pc-transaction',
+  templateUrl: './transaction.component.html',
+  styleUrl: './transaction.component.scss'
+})
+export class TransactionComponent {
+
+}
