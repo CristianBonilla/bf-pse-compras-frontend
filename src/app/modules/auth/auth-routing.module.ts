6fa83d0f0013278b500fa719c7bfbcd1cb2f290c
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2024-10-08 17:41:37 -0500

    feat: :art: Form content component for each transaction flow

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthComponent } from './auth.component';
+import { AuthComponent } from '@module/auth/auth.component';
+import { LoginComponent } from '@module/auth/login/login.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: AuthComponent
+    component: AuthComponent,
+    children: [
+      {
+        path: '',
+        component: LoginComponent
+      },
+      {
+        path: 'login',
+        redirectTo: '',
+        pathMatch: 'prefix'
+      }
+    ]
   }
 ];
 
--- src/app/modules/layout/content/content.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'bf-pc-content',
-  templateUrl: './content.component.html',
-  styles: ``
-})
-export class ContentComponent { }
