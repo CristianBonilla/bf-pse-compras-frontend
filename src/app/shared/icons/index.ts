f3970eaa8b09e5ba3f686fb509515c1478538b68
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2024-10-08 11:04:39 -0500

    :construction: Changes login component in progress

--- src/app/shared/icons/index.ts
+++ src/app/shared/icons/index.ts
@@ -3,4 +3,4 @@ export {
   faWhatsapp,
   faYoutube
 } from '@fortawesome/free-brands-svg-icons';
-export { faSpinner } from '@fortawesome/free-solid-svg-icons';
+export { faAngleRight, faSpinner } from '@fortawesome/free-solid-svg-icons';
