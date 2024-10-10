4be10a43500e31aa5c5b80f84192be5a9c4b6ff1
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2024-10-10 14:04:36 -0500

    :construction: Stepper component is created successfully

---
+++ src/app/modules/content/components/stepper/stepper.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bf-pc-stepper',
+  templateUrl: './stepper.component.html',
+  styles: ``
+})
+export class StepperComponent { }
