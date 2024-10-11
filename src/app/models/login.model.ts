4b95f752cbcff90908da9791330955a002ea050f
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2024-10-11 14:12:09 -0500

    feat: :art: Selection element is completed in login component

--- src/app/models/login.model.ts
+++ src/app/models/login.model.ts
@@ -3,8 +3,8 @@ import { DOCUMENT_TYPE } from '@shared/constants/document.constants';
 import { PERSON_TYPE } from '@shared/constants/person.constants';
 
 export interface LoginForm {
-  personType: FormControl<typeof PERSON_TYPE[number] | null>;
-  documentType: FormControl<typeof DOCUMENT_TYPE[number] | null>;
+  personType: FormControl<typeof PERSON_TYPE[number]>;
+  documentType: FormControl<typeof DOCUMENT_TYPE[number]>;
   documentNumber: FormControl<string | null>;
   internetKey: FormControl<string | null>;
 };
