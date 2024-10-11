a4b562b5cb6ff04e3dde4921260141223a2b5d0b
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2024-10-11 13:07:47 -0500

    refactor: :recycle: Form types and constants are refactored

--- src/app/shared/constants/document.constants.ts
+++ src/app/shared/constants/document.constants.ts
@@ -1,33 +1,40 @@
 import { DocumentType } from '@shared/enums/document.enums';
-import { FormOption } from '@shared/types/form.types';
+import { FormSelectOption } from '@shared/types/form.types';
 
-export const DOCUMENT_TYPE: (FormOption<'Seleccionar tipo de documento', null> | FormOption<string, DocumentType>)[] = [
+export const DOCUMENT_TYPE: [FormSelectOption<'Seleccionar tipo de documento', null>, ...FormSelectOption<string, DocumentType>[]] = [
   {
     text: 'Seleccionar tipo de documento',
-    value: null
+    value: null,
+    selected: true,
   },
   {
     text: 'CC',
-    value: DocumentType.CC
+    value: DocumentType.CC,
+    selected: false
   },
   {
     text: 'NIT',
-    value: DocumentType.NIT
+    value: DocumentType.NIT,
+    selected: false
   },
   {
     text: 'CE',
-    value: DocumentType.CE
+    value: DocumentType.CE,
+    selected: false
   },
   {
     text: 'TI',
-    value: DocumentType.TI
+    value: DocumentType.TI,
+    selected: false
   },
   {
     text: 'PAS',
-    value: DocumentType.PAS
+    value: DocumentType.PAS,
+    selected: false
   },
   {
     text: 'CD',
-    value: DocumentType.CD
+    value: DocumentType.CD,
+    selected: false
   }
 ];
