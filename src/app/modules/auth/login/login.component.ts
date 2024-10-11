14005f08fc8f8b87074d204d9810e21ae7f4fad3
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2024-10-11 17:03:02 -0500

    feat: :construction: Reactive login typed form is added to the component

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { LoginFormGroup } from '@models/form';
 
 @Component({
   selector: 'bf-pc-login',
@@ -8,7 +9,28 @@ import { FormBuilder } from '@angular/forms';
 })
 export class LoginComponent {
   readonly #formBuilder = inject(FormBuilder);
-  readonly loginForm = this.#formBuilder.group({});
+  readonly loginForm = this.#formBuilder.group<LoginFormGroup>({
+    personType: [null],
+    documentType: [null],
+    documentNumber: [null],
+    internetKey: [null]
+  });
+
+  get personType() {
+    return this.loginForm.controls['personType'];
+  }
+
+  get documentType() {
+    return this.loginForm.controls['documentType'];
+  }
+
+  get documentNumber() {
+    return this.loginForm.controls['documentNumber'];
+  }
+
+  get internetKey() {
+    return this.loginForm.controls['internetKey'];
+  }
 
   login() {}
 }
