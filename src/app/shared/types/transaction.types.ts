689ba6914085aadf01be58187c8502f3fc3fbece
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2024-10-17 18:50:26 -0500

    refactor: :recycle: Required selection validator refactor

--- src/app/shared/types/transaction.types.ts
+++ src/app/shared/types/transaction.types.ts
@@ -1,4 +1,3 @@
-import { FormControl } from '@angular/forms';
 import { FormSelectOption } from '@shared/types/form.types';
 
-export type TransactionValue = FormControl<FormSelectOption<'Seleccionar cuenta', null> | FormSelectOption<string, string>>;
+export type TransactionValue = FormSelectOption<'Seleccionar cuenta', null> | FormSelectOption<string, string>;
