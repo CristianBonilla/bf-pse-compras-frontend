08a12ec26a51bda3d81a1507d25343a57f4974ae
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2024-10-22 16:19:46 -0500

    feat: :construction: HTML layout of dynamic key content component

---
+++ src/app/modules/content/components/confirm-dynamic-key/confirm-dynamic-key.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bf-pc-confirm-dynamic-key',
+  templateUrl: './confirm-dynamic-key.component.html',
+  styles: ``
+})
+export class ConfirmDynamicKeyComponent { }
