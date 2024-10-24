6dd8def47c179693ee007b64c435c90931f67505
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2024-10-24 16:17:49 -0500

    :lipstick: Invalid transaction styles adjusted

--- src/app/modules/transaction/invalid/invalid.component.ts
+++ src/app/modules/transaction/invalid/invalid.component.ts
@@ -3,6 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'bf-pc-invalid',
   templateUrl: './invalid.component.html',
-  styles: ``
+  styles: `
+    :host ::ng-deep .content__body {
+      justify-content: flex-start;
+    }
+  `
 })
 export class InvalidComponent { }
