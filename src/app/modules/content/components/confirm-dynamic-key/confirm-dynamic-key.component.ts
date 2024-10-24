1070e1d436f47d626127ec7cb15ebdf5a061fa9c
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2024-10-24 11:06:49 -0500

    refactor: :recycle: Authorize dynamic key component has been rebuilt

--- src/app/modules/content/components/confirm-dynamic-key/confirm-dynamic-key.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'bf-pc-confirm-dynamic-key',
-  templateUrl: './confirm-dynamic-key.component.html',
-  styles: ``
-})
-export class ConfirmDynamicKeyComponent { }
