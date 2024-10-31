9757419a62de1b8b6eb9e9111d7448a6d8b6d657
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2024-10-31 14:47:07 -0500

    feat: :boom: Transaction alert defined for invalid transaction

--- src/app/modules/transaction/invalid/invalid.component.ts
+++ src/app/modules/transaction/invalid/invalid.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { TransactionAlertService } from '@module/content/services/transaction-alert/transaction-alert.service';
+import { TransactionAlert } from '@shared/enums/transaction-alert.enum';
 
 @Component({
   selector: 'bf-pc-invalid',
@@ -9,4 +11,14 @@ import { Component } from '@angular/core';
     }
   `
 })
-export class InvalidComponent { }
+export class InvalidComponent implements OnInit {
+  readonly #transactionAlert = inject(TransactionAlertService);
+  transactionAlert$ = this.#transactionAlert.transactionAlert$;
+
+  ngOnInit() {
+    this.#transactionAlert.updateTransactionAlertFromMessageIndex(
+      TransactionAlert.Danger,
+      'invalid'
+    );
+  }
+}
