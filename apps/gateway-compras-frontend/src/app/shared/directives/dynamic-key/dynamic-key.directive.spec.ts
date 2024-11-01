ed6f745375ea742e465add9e51ad541e541e015a
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2024-11-01 15:53:44 -0500

    feat: :boom: Remaining shared module is added in main monorepo

---
+++ apps/gateway-compras-frontend/src/app/shared/directives/dynamic-key/dynamic-key.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DynamicKeyDirective } from './dynamic-key.directive';
+
+describe('DynamicKeyDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DynamicKeyDirective();
+    expect(directive).toBeTruthy();
+  });
+});
