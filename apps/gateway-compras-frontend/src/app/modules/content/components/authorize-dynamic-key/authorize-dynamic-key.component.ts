a5265664a5c712131f5729c311ca412c7582ad29
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2024-11-01 15:55:09 -0500

    feat: :boom: Content module is added to main monorepo

---
+++ apps/gateway-compras-frontend/src/app/modules/content/components/authorize-dynamic-key/authorize-dynamic-key.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bf-pc-authorize-dynamic-key',
+  templateUrl: './authorize-dynamic-key.component.html',
+  styles: ``
+})
+export class AuthorizeDynamicKeyComponent { }
---
+++ apps/gateway-compras-frontend/src/app/modules/content/content.module.ts
@@ -0,0 +1,44 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthorizeDynamicKeyComponent } from '@module/content/components/authorize-dynamic-key/authorize-dynamic-key.component';
+import { DynamicKeyComponent } from '@module/content/components/dynamic-key/dynamic-key.component';
+import { FormComponent } from '@module/content/components/form/form.component';
+import { LoaderComponent } from '@module/content/components/loader/loader.component';
+import { SelectComponent } from '@module/content/components/select/select.component';
+import { StepperComponent } from '@module/content/components/stepper/stepper.component';
+import { ContentComponent } from '@module/content/content.component';
+import { LoaderService } from '@module/content/services/loader/loader.service';
+import { StepperService } from '@module/content/services/stepper/stepper.service';
+import { TransactionAlertService } from '@module/content/services/transaction-alert/transaction-alert.service';
+import { IconsModule } from '@shared/icons/icons.module';
+
+@NgModule({
+  declarations: [
+    ContentComponent,
+    FormComponent,
+    StepperComponent,
+    SelectComponent,
+    DynamicKeyComponent,
+    AuthorizeDynamicKeyComponent,
+    LoaderComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    IconsModule
+  ],
+  exports: [
+    FormsModule,
+    ReactiveFormsModule,
+    ContentComponent,
+    FormComponent,
+    SelectComponent,
+    DynamicKeyComponent,
+    AuthorizeDynamicKeyComponent,
+    LoaderComponent
+  ],
+  providers: [StepperService, LoaderService, TransactionAlertService]
+})
+export class ContentModule { }
