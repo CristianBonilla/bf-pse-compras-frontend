917e583d85dc0acd89b8b6535ef81882e6ffaa4e
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2024-11-01 15:54:53 -0500

    feat: :boom: Layout module is added to main monorepo

---
+++ apps/gateway-compras-frontend/src/app/modules/layout/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bf-pc-header',
+  templateUrl: './header.component.html',
+  styles: ``
+})
+export class HeaderComponent { }
---
+++ apps/gateway-compras-frontend/src/app/modules/layout/sidebar/sidebar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bf-pc-sidebar',
+  templateUrl: './sidebar.component.html',
+  styles: ``
+})
+export class SidebarComponent { }
