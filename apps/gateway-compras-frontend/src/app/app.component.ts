d8b5c6f445a8821b9c0bfee6dcaed3cc7a31f521
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2024-11-13 13:12:39 -0500

    feat: :boom: App module is added to main monorepo

--- apps/gateway-compras-frontend/src/app/app.component.ts
+++ apps/gateway-compras-frontend/src/app/app.component.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
-  standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
   selector: 'bf-pc-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styles: ``
 })
-export class AppComponent {
-  title = 'gateway-compras-frontend';
-}
+export class AppComponent { }
