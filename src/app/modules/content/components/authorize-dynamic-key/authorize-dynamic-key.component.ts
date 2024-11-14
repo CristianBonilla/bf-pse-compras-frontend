7e9ff0d037da7f91fbffa6c70d62348207420e8a
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2024-11-13 19:38:09 -0500

    feat: :fire: Directories and files removed for restructuring

--- src/app/modules/content/components/authorize-dynamic-key/authorize-dynamic-key.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'bf-pc-authorize-dynamic-key',
-  templateUrl: './authorize-dynamic-key.component.html',
-  styles: ``
-})
-export class AuthorizeDynamicKeyComponent { }
--- src/app/shared/enums/stepper.enums.ts
+++
@@ -1,5 +0,0 @@
-export enum Flow {
-  DefinitionPay = 1,
-  Confirm,
-  Voucher
-};
